f94ead7b2bf23dcb4ee058df4576cbb5a2861bc6
John Mettraux <jmettraux@gmail.com>
2016-03-01 17:05:10 +0900

    implement server

--- lib/server.rb
+++ lib/server.rb
@@ -4,6 +4,29 @@ require 'db'
 
 get '/' do
 
-  'hello world'
+  content_type 'text/plain'
+
+  DB[:items]
+    .all
+    .collect { |i| "#{i[:id]}: #{i[:name].inspect}" }
+    .join("\n")
+end
+
+get '/add/:name' do
+
+  DB[:items].insert(name: params[:name])
+
+  content_type 'text/plain'
+
+  "added #{params[:name].inspect}"
+end
+
+get '/clear' do
+
+  DB[:items].delete
+
+  content_type 'text/plain'
+
+  'cleared.'
 end
 
